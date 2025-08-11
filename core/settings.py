b6d23df1078a76039ac10ecbcaef6411181402af
athulya <athulyaathu.uk@gmail.com>
2025-08-11 17:38:33 +0530

    removed

--- core/settings.py
+++ core/settings.py
@@ -254,19 +254,3 @@ CSRF_COOKIE_SECURE = not DEBUG
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
